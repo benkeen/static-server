70d540c3db900b456ba79464abf64e9aa36a9a52
Ben Keen <ben.keen@gmail.com>
2022-07-11 19:24:55 -0700

    adding CORS support

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
  */
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(cors());
 
 // serve static assets normally
 app.use(express.static(__dirname + '/public'));
